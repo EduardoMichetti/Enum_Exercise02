1fcc7c0708aab714b4fe57dc6b8843becca5679c
Eduardo Michetti <94409995+EduardoMichetti@users.noreply.github.com>
2024-02-28 19:39:26 -0300

    Add project files.

---
+++ Exercise02/Program.cs
@@ -0,0 +1,29 @@
+﻿using Exercise02.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            Post p1 = new Post(
+               DateTime.Parse("21/06/2018 13:05:44"),
+               "Viajando para Nova Zelandia",
+               "Eu vou visitar uma cidade maravilhosa",
+               12
+                );
+            Comment c1 = new Comment("Tenha um bom dia");
+            p1.AddComment(c1);
+            Comment c2 = new Comment("Uau que incrível");
+            p1.AddComment(c2);
+
+            Console.WriteLine(p1);
+            
+        }
+    }
+}
